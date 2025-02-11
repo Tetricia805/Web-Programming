5988aa0a1ce9601c2a8ebd89d7b495f79505d559
Tetricia805 <tetriciag@gmail.com>
2025-02-11 17:22:24 +0300

    Create asynchronous.js

---
+++ Backend/asynchronous.js
@@ -0,0 +1,10 @@
+
+console.log('put water to boil'); //
+
+console.log('boiling water');
+setTimeout (()=>{
+    console.log('The water is ready!');
+}, 5000)
+
+console.log('doing other things');
+
