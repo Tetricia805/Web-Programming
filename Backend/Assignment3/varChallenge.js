82a56ba7b6f588302fc5ab6a19c64789b819e78e
Tetricia805 <tetriciag@gmail.com>
2025-02-24 14:43:05 +0300

    Update varChallenge.js

--- Backend/Assignment3/varChallenge.js
+++ Backend/Assignment3/varChallenge.js
@@ -5,3 +5,12 @@ function calculateProduct(num1, num2){
 }
 result = calculateProduct(2,5);
 console.log(result);
+
+
+// function that calculates the multiplication of five
+let num = 10
+function multiplication_of_five(num){
+    return num*5
+}
+result = multiplication_of_five(5);
+console.log(result);
